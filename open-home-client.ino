2bc63b01c2098ec14c215132c6e9c0a9d51da052
Kevin Hoogendijk <Kevin Hoogendijk>
2017-01-04 18:42:59 +0100

    Adds getstatus command, simple analog and digital

--- open-home-client.ino
+++ open-home-client.ino
@@ -1,6 +1,8 @@
-#define NBR_MODULES 1
+#define NBR_TGGL 1
+#define NBR_SNSR 0
 
-int modules[NBR_MODULES] = {13};
+int toggle[NBR_TGGL] = {13};
+int sensor[NBR_SNSR] = {};
 
 typedef struct {
   uint16_t pin;
@@ -9,27 +11,41 @@ typedef struct {
 
 void setup() {
   Serial.begin(115200);
-  for (int i = 0; i < NBR_MODULES; i++) {
-    pinMode(modules[i], OUTPUT);  
+
+  // Initializes toggle modules
+  for (int i = 0; i < NBR_TGGL; i++) {
+    pinMode(toggle[i], OUTPUT);  
   }
-  digitalWrite(13, LOW);
+
+  // Initializes sensor modules
+  for (int i = 0; i < NBR_SNSR; i++) {
+    pinMode(sensor[i], INPUT);
+  }
 }
 
 Command cmd;
 boolean success;
+String value; 
 
 void loop() {
+  value = "";
   cmd = readCommand();
   if (*cmd.action == 'p') {
     success = on(cmd.pin);
   } else if (*cmd.action == 'a') {
     success = off(cmd.pin);
+  } else if (*cmd.action == 's') {
+    value = getStatus(cmd.pin);
   }
-  
-  if (success) {
-    Serial.print("True");
+
+  if (value != "") {
+    Serial.print(value + ";");
   } else {
-    Serial.print("False");
+    if (success) {
+      Serial.print("True");
+    } else {
+      Serial.print("False");
+    }
   }
 }
 
@@ -43,16 +59,47 @@ Command readCommand() {
   };
 }
 
+// Checks what type a pin is, returns -1 if the pin is not initialized
+int typeOfPin(int pin) {
+  for (int i = 0; i < NBR_TGGL; i++) {
+    if (toggle[i] == pin) {
+      return 0;
+    }
+  }
+
+  for (int i = 0; i < NBR_SNSR; i++) {
+    if (sensor[i] == pin) {
+      return 1;
+    }
+  }
+
+  return -1;
+}
+
+String getStatus(int pin) {
+  switch (typeOfPin(pin)) {
+    case -1: return String(-1); 
+    case 0: return String(statusDigital(pin)); 
+    case 1: return String(statusAnalog(pin)); 
+  }
+}
+
 // A couple of functions to get info and set pins on the arduino
 
 // Sets pin to HIGH
 boolean on(int pin) {
+  if (typeOfPin(pin) != 0) {
+    return false;
+  }
   digitalWrite(pin, HIGH);
   return digitalRead(pin) == HIGH;
 }
 
 // Sets pin to LOW
 boolean off(int pin) {
+  if (typeOfPin(pin) != 0) {
+    return false;
+  }
   digitalWrite(pin, LOW);
   return digitalRead(pin) == LOW;
 }
@@ -60,4 +107,9 @@ boolean off(int pin) {
 // Reads the value of a simple passive analog sensor
 float statusAnalog(int pin) {
   return analogRead(pin);
+}
+
+// Reads the digital value of an output pin
+int statusDigital(int pin) {
+  return digitalRead(pin);
 }
